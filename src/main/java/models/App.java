023426f0574a1f5eadd58e3a56bed477e4aa7c21
Ephraim19 <machayoephraim@gmail.com>
2022-03-09 08:07:13 +0300

    Added templates

---
+++ src/main/java/models/App.java
@@ -0,0 +1,17 @@
+package models;
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static spark.Spark.get;
+
+public class App {
+    public static void main(String[] args) {
+        get("/",(request, response) -> {
+            Map<String,Object> model=new HashMap<String, Object>();
+            return new ModelAndView(model,"index.hbs");
+        },new HandlebarsTemplateEngine());
+    }
+}
