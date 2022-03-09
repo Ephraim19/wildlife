dc84f121f765dcb724314ecfc7e757d3c7c27203
Ephraim19 <machayoephraim@gmail.com>
2022-03-09 10:51:51 +0300

    Created routes for collecting data

--- src/main/java/models/App.java
+++ src/main/java/models/App.java
@@ -6,6 +6,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 import static spark.Spark.get;
+import static spark.Spark.post;
 
 public class App {
     public static void main(String[] args) {
@@ -26,9 +27,21 @@ public class App {
             return new ModelAndView(model,"endangered-animal.hbs");
         },new HandlebarsTemplateEngine());
 
-        //add sightings
-        get("sighting",(request, response) -> {
+        //add animal
+        post("/sighting/animal",(request, response) -> {
             Map<String,Object> model=new HashMap<String, Object>();
+            String animalName = request.queryParams("name");
+            model.put("animal",animalName);
+            return new ModelAndView(model,"sighting.hbs");
+        },new HandlebarsTemplateEngine());
+
+        //add endangered animal
+        post("/sighting/endangered",(request, response) -> {
+            Map<String,Object> model=new HashMap<String, Object>();
+            String animalName = request.queryParams("name");
+            String age = request.queryParams ("age");
+            String health = request.queryParams("health");
+            System.out.println(age);
             return new ModelAndView(model,"sighting.hbs");
         },new HandlebarsTemplateEngine());
     }
